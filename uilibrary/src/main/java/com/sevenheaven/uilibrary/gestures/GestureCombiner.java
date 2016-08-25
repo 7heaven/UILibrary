c17898cbe3911df6beb61168fb13ab1e84699407
7heaven <caifangmao8@gmail.com>
2016-08-25 11:56:56 +0800

    Add GestureCombiner

---
+++ uilibrary/src/main/java/com/sevenheaven/uilibrary/gestures/GestureCombiner.java
@@ -0,0 +1,11 @@
+package com.sevenheaven.uilibrary.gestures;
+
+import java.util.List;
+
+/**
+ * Created by 7heaven on 16/8/22.
+ */
+public class GestureCombiner {
+
+    private List<BaseGestureRecognizer> recognizers;
+}
